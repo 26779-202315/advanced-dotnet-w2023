7733d558acf16e5a55e2d4d840e5f4916cb1b69c
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2023-02-09 18:12:52 -0500

    Week05-P2

---
+++ Week05_2_ErrorHandling/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Week05_2_ErrorHandling
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await GetSiteLengthTaskAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main 'Task' Catch: {ex.Message}");
+            }
+
+
+            try
+            {
+                GetSiteLengthVoidAsync(); //we cannot use await on an async void method
+
+                //Since this method uses void, then No 'Task' object means the exception will not be caught in the calling method
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main 'void' Catch: {ex.Message}");
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("All Done!");
+
+        }
+
+        /// <summary>
+        /// Starts multiple asynchronous tasks, to contact multiple websites
+        /// </summary>
+        /// <remarks>we are not explicitly returning anything from this method</remarks>
+        private static async Task GetSiteLengthTaskAsync()
+        {
+            ////Error generating list because the "invalid website"
+            var siteList = new List<string> { "yahoo", "invalid website", "google", "msn", "reddit" };
+
+            foreach (string site in siteList)
+            {
+                var task = client.GetStringAsync($"http://{site}.com");
+
+                await task;
+                Console.WriteLine($"{site} content length is {task.Result.Length}");
+            }
+        }
+
+        /// <summary>
+        /// Starts multiple asynchronous tasks, to contact multiple websites
+        /// </summary>
+        /// <remarks>'async void' should be avoided (use 'async Task' instead), except for event handlers</remarks>
+        private static async void GetSiteLengthVoidAsync()
+        {
+            ////Error generating list because the "invalid website"
+            var siteList = new List<string> { "yahoo", "invalid website", "google", "msn", "reddit" };
+
+            foreach (string site in siteList)
+            {
+                var task = client.GetStringAsync($"http://{site}.com");
+
+                await task;
+                Console.WriteLine($"{site} content length is {task.Result.Length}");
+            }
+        }
+    }
+
+}
---
+++ Week05_3_MultiTask_WhenAny/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace Week05_3_MultiTask_WhenAny
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await GetSiteLengthTaskAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main 'Task' Catch: {ex.Message}");
+            }
+
+            try
+            {
+                await GetSiteLengthAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Main 'Task' Catch: {ex.Message}");
+            }
+
+
+            Console.WriteLine("All Done!");
+
+        }
+
+
+        /// <summary>
+        /// Starts multiple asynchronous tasks, to contact multiple websites
+        /// </summary>
+        /// <remarks>we are not explicitly returning anything from this method</remarks>
+        private static async Task GetSiteLengthAsync()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+            var siteList = new List<string> { "yahoo", "google", "msn", "reddit" };
+
+            //Using LINQ to create and invoke multiple tasks using the sitelist object
+            List<Task<string>> taskList = (from site in siteList
+                                           select client.GetStringAsync($"http://{site}.com")).ToList();
+
+            int sumLength = 0;
+            Console.WriteLine("Starting Loop!!");
+
+            while (taskList.Any()) //Check if there is still any (pending) tasks in the tasklist
+            {
+                //Retrieve the task object for whichever task that finishes first; there is no guarantee of what order this will be. as a matter of fact, if you run this multiple times; you are more than likely to get different order every few times
+                var firstToFinsh = await Task.WhenAny(taskList);
+                Console.WriteLine($"content length is {firstToFinsh.Result.Length}");
+
+                sumLength += firstToFinsh.Result.Length;
+                //remove completed tasks
+                taskList.Remove(firstToFinsh);
+            }
+
+            Console.WriteLine($"Total length: {sumLength}");
+
+            var time = watch.ElapsedMilliseconds;
+            Console.WriteLine($"ms: {time}");
+        }
+
+
+        /// <summary>
+        /// Starts multiple asynchronous tasks, to contact multiple websites
+        /// </summary>
+        /// <remarks>we are not explicitly returning anything from this method</remarks>
+        private static async Task GetSiteLengthTaskAsync()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+
+            var siteList = new List<string> { "yahoo", "google", "msn", "reddit" };
+
+            foreach (string site in siteList)
+            {
+                var task = client.GetStringAsync($"http://{site}.com");
+
+                await task;
+                Console.WriteLine($"{site} content length is {task.Result.Length}");
+            }
+
+            var time = watch.ElapsedMilliseconds;
+            Console.WriteLine($"ms: {time}");
+        }
+
+    }
+
+}
